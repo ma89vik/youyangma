54d606d7e70168e23d5bbdfa9ba297616e5052da
Marius <ma89vik@gmail.com>
2022-04-09 18:40:44 +0800

    Add initial backend files

---
+++ youyangma_back/index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const axios = require('axios')
+const cors = require('cors')
+
+const infected_search_url = "https://chenfan.info/iframe_search/%E5%88%B6%E9%80%A0%E5%B1%80%E8%B7%AF567"
+
+const getData = async (url) => {
+  try {
+    const response = await axios.get(url)
+    const data = response.data
+    console.log(data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const positive_cases = [
+    {
+        date: "2022-04-08"
+    },
+    {
+        date: "2022-04-07"
+    },
+    {
+        date: "2022-04-06"
+    },
+    {
+        date: "2022-04-04"
+    },
+];
+
+app.use(cors())
+
+app.get('/api/yang', (request, response) => {
+  response.json(positive_cases)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
